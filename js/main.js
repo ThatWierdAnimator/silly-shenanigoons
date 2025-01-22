edabdd8ed0dfb2c804ba0e8ca8b87447ecb1f1f6
Schroeder Ryan <142847750+ThatWierdAnimator@users.noreply.github.com>
2025-01-22 10:19:53 -0800

    location data lol

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
-let storedLatitude = localStorage.getItem('latitude');
-let storedLongitude = localStorage.getItem('longitude');
-
 // This function alerts the user of their coordinates (in case they forgot)
 function showCoords(latitude, longitude) {
     if (!latitude && !longitude) {
         alert('No coordinates found (you are safe)');
     } else {
-        alert(`${latitude}N ${longitude}W`);
+        alert(`${latitude}, ${longitude}`);
     }
-    
 }
 
 // This function grabs and saves the users coordinates
 // This prompts when the site is opened
 navigator.geolocation.getCurrentPosition((position) => {
-    localStorage.setItem("latitude", latitude);
-    localStorage.setItem("longitude", longitude);
+    localStorage.setItem("latitude", position.coords.latitude);
+    localStorage.setItem("longitude", position.coords.longitude);
 });
 
 // This function will update the stored coords whenever its called
 function updateCoords() {
     navigator.geolocation.getCurrentPosition((position) => {
-        localStorage.setItem("latitude", latitude);
-        localStorage.setItem("longitude", longitude);
+        localStorage.setItem("latitude", position.coords.latitude);
+        localStorage.setItem("longitude", position.coords.longitude);
     });
 }
 
 // Shows location when the show location button is clicked
 document.getElementById('showLocation').addEventListener('click', () => {
-    showCoords(storedLatitude, storedLongitude);
+    showCoords(localStorage.getItem('latitude'), localStorage.getItem('longitude'));
 });
 
 document.getElementById('updateLocation').addEventListener('click', () => {
     updateCoords();
+});
+
+// Removes location from local storage
+document.getElementById('removeLocation').addEventListener('click', () => {
+    localStorage.removeItem('latitude');
+    localStorage.removeItem('longitude');
 });
