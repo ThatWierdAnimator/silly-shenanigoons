7f4caa10a0a8d4019f3ccb6bf8d11d75c8e038ce
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-01-23 10:43:13 -0800

    psuhhhhhhhhhh

--- js/main.js
+++ js/main.js
@@ -36,3 +36,13 @@ document.getElementById('removeLocation').addEventListener('click', () => {
     localStorage.removeItem('latitude');
     localStorage.removeItem('longitude');
 });
+
+
+
+let iframe = document.getElementById('maps');
+
+let lat = localStorage.getItem('latitude');
+let long = localStorage.getItem('longitude');
+
+iframe.setAttribute('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509167!2d144.95373531531506!3d-37.81720997975187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f123f5b%3A0x5045675218ceed28!2s${}%20${}!5e0!3m2!1sen!2sus!4v1616730255295!5m2!1sen!2sus');
+
